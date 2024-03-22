ae3a8331a765d525beb00c9ec7b1658c1193d234
Berke-Celikoz <berke.celikozitu@gmail.com>
2024-03-22 09:23:40 +0300

    new file:   Kitapfirsati.com.py 	new file:   Kitapfirsati_data.py 	modified:   kitapfirsati.ipynb 	new file:   scrap.ipynb 	new file:   scraped_data.csv 	new file:   templates/index.html

---
+++ Kitapfirsati.com.py
@@ -0,0 +1,36 @@
+"""from flask import Flask, render_template
+import pandas as pd
+
+app = Flask(__name__)
+
+# Load the scraped data from the CSV file
+df = pd.read_csv('scraped_data.csv')
+
+@app.route('/')
+def index():
+    # Render the template with the unsorted data
+    return render_template('index.html', books= df.to_dict(orient='records'))
+
+if __name__ == '__main__':
+    app.run(debug=True)"""
+from flask import Flask, render_template
+import pandas as pd
+
+app = Flask(__name__)
+
+# Load the scraped data from the CSV file
+df = pd.read_csv('scraped_data.csv')
+
+@app.route('/')
+def index():
+    # Convert discount percentage column to numeric values
+    df['Discount Percentage'] = df['Discount Percentage'].astype(float)
+    
+    # Sort the data by discount percentage in descending order
+    df_sorted = df.sort_values(by='Discount Percentage', ascending=False)
+    
+    # Render the template with the sorted data
+    return render_template('index.html', books=df_sorted.to_dict(orient='records'))
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ Kitapfirsati_data.py
@@ -0,0 +1,59 @@
+import pandas as pd
+from bs4 import BeautifulSoup
+import requests
+
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36",
+    "Accept-Encoding": "gzip, deflate",
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+    "DNT": "1", "Connection": "close", "Upgrade-Insecure-Requests": "1"
+}
+
+data = []
+
+for page_number in range(1, 331):
+    url = f"https://www.bkmkitap.com/edebiyat-kitaplari?pg={page_number}"
+    page = requests.get(url, headers=headers).text  # connect to the website
+    soup = BeautifulSoup(page, "html.parser")
+    product_items = soup.find_all("div", class_="col col-3 col-md-4 col-sm-6 col-xs-6 p-right mb productItem zoom ease")
+    print(f"Scraping page {page_number}...")
+
+    # Iterate over each product on the current page
+    for i in product_items:
+        # Create a dictionary to store the scraped data for each product
+        product_data = {}
+
+        # Extracting book name
+        product_data['Book Name'] = i.find("a", class_="fl col-12 text-description detailLink").text.strip()
+
+        # Extracting publisher
+        product_data['Publisher'] = i.find("a", class_="col col-12 text-title mt").text.strip()
+
+        # Extracting author
+        product_data['Author'] = i.find("a", class_="fl col-12 text-title").text.strip()
+
+        # Extracting discounted price
+        try:
+            product_data['Discounted Price'] = i.find("div", class_="text-line discountedPrice").text.strip().split("\n")[0].strip()
+        except AttributeError:
+            product_data['Discounted Price'] = None
+        try:
+             # Extracting discount percentage
+            product_data['Discount Percentage'] = i.find("div", class_="text-line discountedPrice").find("span").text.strip()[1:4]
+            print(i.find("div", class_="text-line discountedPrice").find("span").text.strip()[1:3])
+        except AttributeError:
+            product_data['Discounted Percentage'] = None
+
+        new_price = i.find("div", class_="currentPrice").text.strip().split("\n")[0].strip()
+        product_data['New Price'] = new_price.replace('TL', '')
+
+        # Append the dictionary to the list
+        data.append(product_data)
+
+# Convert the list of dictionaries to a DataFrame
+df = pd.DataFrame(data)
+
+# Save the DataFrame to a CSV file
+df.to_csv('scraped_data.csv', index=False)
+
+print("Data saved to 'scraped_data.csv'")
