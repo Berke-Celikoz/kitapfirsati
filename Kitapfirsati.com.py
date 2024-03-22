07f4a891624f406f187762c860e2435126547b5e
Berke-Celikoz <berke.celikozitu@gmail.com>
2024-03-22 09:42:24 +0300

    modified:   Kitapfirsati.com.py 	modified:   scraped_data.csv

--- Kitapfirsati.com.py
+++ Kitapfirsati.com.py
@@ -1,18 +1,3 @@
-"""from flask import Flask, render_template
-import pandas as pd
-
-app = Flask(__name__)
-
-# Load the scraped data from the CSV file
-df = pd.read_csv('scraped_data.csv')
-
-@app.route('/')
-def index():
-    # Render the template with the unsorted data
-    return render_template('index.html', books= df.to_dict(orient='records'))
-
-if __name__ == '__main__':
-    app.run(debug=True)"""
 from flask import Flask, render_template
 import pandas as pd
 
